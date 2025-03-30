25d03f77ed2aad5bb305b3d45c8f95f7f32b6a21
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-03-30 12:59:50 -0500

    Initial commit

---
+++ Block07/block07.js
@@ -0,0 +1,13 @@
+ //First create console log
+ console.log("Hello")
+
+ //use const to create string
+ const greeting = "You have recieved this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+    //use let to create string
+    let combination = 5+5
+    let combination2 = 20*2
+    let combination3 = 40-1
+
+    //create alert
+    alert(greeting + combination "Space"+ combination2 + combination3)
